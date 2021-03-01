10901330024922a143ae056e840dfdda85526045
ludwin1 <ludwinkeller4@gmail.com>
2021-03-01 17:45:02 -0600

    01-marzo-2021

---
+++ src/main/java/mx/unam/aragon/fes/Direccion.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.unam.aragon.fes;
+
+/**
+ *
+ * @author Mireya
+ */
+public class Direccion {
+   private String calle;
+   private String numero;
+   private String colonia;
+   private String delegacion;
+   private String estado;
+   private String cp;
+   
+   public Direccion(){
+   }
+}
